a20123b2113e732e9036237a604740402959c348
elenaGaV8 <132475709+elenaGaV8@users.noreply.github.com>
2023-05-10 16:42:37 +0300

    Update Игра Пинг-понг.py
    законченная версия

--- Игра Пинг-понг.py
+++ Игра Пинг-понг.py
@@ -1,5 +1,7 @@
 from pygame import *
-
+mixer.init()
+font.init()
+from time import time as timer
 RED = (255, 0, 0)
 GREEN = (0, 255, 51)
 BlUE = (0, 0, 255)
@@ -11,11 +13,9 @@ LIGHT_RED = (250, 128, 114)
 BLACK = (0, 0, 0)
 DARK_BLUE = (0, 0, 100)
 LIGHT_BLUE = (80, 80, 255)
-
 window = display.set_mode((700, 500))
-display.set_caption('Игра Пинг-Понг')
+display.set_caption('')
 window.fill(LIGHT_BLUE)
-
 
 
 clock = time.Clock()
@@ -37,17 +37,25 @@ class Player(GameSprite):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 25:
             self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < 200:
+        if keys_pressed[K_s] and self.rect.y < 350:
             self.rect.y += self.speed
     def update_r(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y > 25:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 200:
+        if keys_pressed[K_DOWN] and self.rect.y < 350:
             self.rect.y += self.speed
-player_1 = Player('левая платформа.png', 50, 100, 4, 100, 300)
-player_2 = Player('правая платформа.png', 550,100, 4, 100, 300)
-ball = GameSprite('ball.png', 100, 100, 4, 50, 50)
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y >= 500:
+            self.rect.y = 0
+            self.rect.x = randint(30,650)
+            self.rect.x += self.speed
+player_1 = Player('левая платформа.png', 50, 300, 4, 50, 150)
+player_2 = Player('правая платформа.png', 550, 300, 4, 50, 150)
+ball = Enemy('ball.png', 100, 100, 4, 50, 50)
 speed_x = 3
 speed_y =3
 game = True
@@ -71,11 +79,25 @@ while game:
         player_1.reset()
         player_2.reset()
         player_2.update_r()
-        '''ball.rect.x += speed_x
+        ball.rect.x += speed_x
         ball.rect.y += speed_y
-        if ball.rect.y > height-50 r ball.rect..'''
+        
 
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            speed_y*=-1
+        if sprite.collide_rect(ball, player_1) or sprite.collide_rect(ball, player_2):
+            speed_x *= -1
+        #ball.update()
 
+        ball.reset()
+        if ball.rect.x > 650:
+            finish = True
+            win1 = font.SysFont('Arial', 30).render('Игрок 1 выиграл!!!', 1 , (255, 255, 255))
+            window.blit(win1, (100, 100))
+        if ball.rect.x < 0:
+            finish = True
+            win2 = font.SysFont('Arial', 30).render('Игрок 2 выиграл!!!', 1 , (255, 255, 255))
+            window.blit(win2, (100, 100))    
     
     
     clock.tick(60)
